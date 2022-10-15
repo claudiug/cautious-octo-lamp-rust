82d28813ec2edb11b4ea0851ab76aa9ada41800c
CGI <claudiu.garba@gmail.com>
2022-10-15 22:29:38 +0200

    menu

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,61 @@
 use bracket_lib::prelude::*;
+enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
 
-struct State {}
+struct State {
+    mode: GameMode,
+}
+
+impl State {
+    fn new() -> Self {
+        State {
+            mode: GameMode::Menu,
+        }
+    }
+
+    fn play(&mut self, ctx: &mut BTerm) {
+        self.mode = GameMode::End;
+    }
+
+    fn dead(&self, ctx: &mut BTerm) {
+        todo!()
+    }
+
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "welcome to game");
+        ctx.print_centered(8, "(P) play");
+        ctx.print_centered(9, "(Q) quit play");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+
+    fn restart(&mut self) {
+        self.mode = GameMode::Playing;
+    }
+}
+
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-        ctx.print(1, 1, "Hello");
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::End => self.dead(ctx),
+            GameMode::Playing => self.play(ctx),
+        }
     }
 }
 
 fn main() -> BError {
     let context = BTermBuilder::simple80x50().with_title("title").build()?;
 
-    main_loop(context, State{})
+    main_loop(context, State::new())
 }
